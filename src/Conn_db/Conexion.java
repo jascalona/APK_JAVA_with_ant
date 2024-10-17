c4524e1517d234024c18b1b5919b787f6fc15f71
unknown <JEscalona@Des.lan.xven.com.ve>
2024-10-17 10:11:57 -0400

    new repositi carga

---
+++ src/Conn_db/Conexion.java
@@ -0,0 +1,16 @@
+package Conn_db;
+
+/**
+ *
+ * @author JEscalona
+ */
+
+import javax.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion {
+    
+}
---
+++ src/phonebook/PhoneBook.java
@@ -0,0 +1,22 @@
+package phonebook;
+
+/**
+ *
+ * @author JEscalona
+ */
+
+
+
+public class PhoneBook {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        interface_i inter_i =new interface_i();
+        inter_i.setVisible(true);
+        
+    }
+    
+}
